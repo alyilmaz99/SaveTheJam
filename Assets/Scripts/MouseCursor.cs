f00ffddadcee5aa2ec893112d78bda2a5086b0e9
alyilmaz99 <aliyilmaz9977@gmail.com>
2022-12-11 01:13:33 +0300

    last version of stable vs
    son haline geri getirildi

--- Assets/Scripts/MouseCursor.cs
+++ Assets/Scripts/MouseCursor.cs
@@ -1,4 +1,5 @@
-using System.Collections;
+
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -42,6 +43,52 @@ public class MouseCursor : MonoBehaviour
                 parentObject = hit.collider.gameObject.transform.parent.gameObject;
             }
             else if(parentObject!=null) {
+                parentObject.GetComponent<GroundStats>().mouseCheck = false;
+            }
+        }
+
+    }
+}
+*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+
+    public LayerMask mask;
+    [SerializeField] private GameObject parentObject;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        MouseClick();
+
+    }
+
+    void MouseClick()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("ilk");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit;
+
+            if (Physics.Raycast(ray, out hit, mask))
+            {
+                Debug.Log("ikii");
+                hit.collider.gameObject.GetComponentInParent<GroundStats>().mouseCheck = true;
+                parentObject = hit.collider.gameObject.transform.parent.gameObject;
+            }
+            else
+            {
                 parentObject.GetComponent<GroundStats>().mouseCheck = false;
             }
         }
