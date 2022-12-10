8e9a20e30082a7a69cf9b85ef22aaa9196d322c1
Ayberk Keskin <ayberkeskn@gmail.com>
2022-12-10 05:17:29 +0300

    Add Images

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,12 +6,18 @@ public class GameController : MonoBehaviour
 {
 
     [SerializeField] List<GameObject> Grounds;
-    
-    
+    [SerializeField] ProfilePanelController profilePanelController;
+    [SerializeField] public int Gold;
+    [SerializeField] public int Science;
+    [SerializeField] public int Military;
+    [SerializeField] public string State;
 
     void Start()
     {
-        
+        profilePanelController.GoldText.text = Gold.ToString();
+        profilePanelController.ScienceText.text = Science.ToString();
+        profilePanelController.Militarytext.text = Military.ToString();
+        profilePanelController.StageText.text = State;
     }
 
     
--- Assets/Scripts/ProfilePanelController.cs
+++ Assets/Scripts/ProfilePanelController.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.IO;
 
 public class ProfilePanelController : MonoBehaviour
 {
-
+    [SerializeField] GameController gameController;
     [SerializeField] public Text StageText;
     [SerializeField] public Image StageImage;
     [SerializeField] public Text GoldText;
@@ -15,13 +15,44 @@ public class ProfilePanelController : MonoBehaviour
     [SerializeField] public Image ScienceImage;
     [SerializeField] public Text Militarytext;
     [SerializeField] public Image MilitaryImage;
-
-
+    [SerializeField] public List<Sprite> Images;
     void Start()
     {
-        
+  
+        switch (gameController.State)
+        {
+            case "1.Çað":
+                StageImage.sprite = Images[0];
+                break;
+            case "2.Çað":
+                StageImage.sprite = Images[1];
+                break;
+            case "3.Çað":
+                StageImage.sprite = Images[2];
+                break;
+            case "4.Çað":
+                StageImage.sprite = Images[3];
+                break;
+            case "5.Çað":
+                StageImage.sprite = Images[4];
+                break;
+            default:
+                break;
+        }
     }
+    public static Texture2D LoadImage(string filePath)
+    {
+        Texture2D tex = null;
+        byte[] fileData;
 
+        if (File.Exists(filePath))
+        {
+            fileData = File.ReadAllBytes(filePath);
+            tex = new Texture2D(2, 2);
+            tex.LoadImage(fileData);
+        }
+        return tex;
+    }
 
     void Update()
     {
