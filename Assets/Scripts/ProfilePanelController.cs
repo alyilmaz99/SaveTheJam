2b2fa86a04a2244a4da3a8733545d1ee4d653eee
Ayberk Keskin <ayberkeskn@gmail.com>
2022-12-10 03:31:23 +0300

    AddProfile
    AddProfilepanel

---
+++ Assets/Scripts/ProfilePanelController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ProfilePanelController : MonoBehaviour
+{
+
+    [SerializeField] public Text StageText;
+    [SerializeField] public Image StageImage;
+    [SerializeField] public Text GoldText;
+    [SerializeField] public Image GoldImage;
+    [SerializeField] public Text ScienceText;
+    [SerializeField] public Image ScienceImage;
+    [SerializeField] public Text Militarytext;
+    [SerializeField] public Image MilitaryImage;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
