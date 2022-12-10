a366cc6541b190adf23b668448b81ba6c35017a3
Ayberk Keskin <ayberkeskn@gmail.com>
2022-12-10 11:56:56 +0300

    MapSelecet
    MapSelecet

---
+++ Assets/Scripts/AtackController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtackController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject selectionPrefab;
+    private GameObject newSelection;
+    public bool isSelected;
+    public int military;
+
+    private void OnMouseDown()
+    {
+        if (newSelection==null)
+        {
+            newSelection = Instantiate(selectionPrefab, transform.position, Quaternion.identity);
+            newSelection.transform.SetParent(gameObject.transform);
+            newSelection.SetActive(false);
+        }
+        isSelected = !isSelected;
+        if (isSelected)
+        {
+            newSelection.SetActive(true);
+        }
+        else
+        {
+            newSelection.SetActive(false);
+        }
+    }
+     void Start()
+    {
+        
+    }
+    void Update()
+    {
+      
+    }
+}
