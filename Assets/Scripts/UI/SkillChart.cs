4074e359c0430d8556f3cfbf6a4b83975c3a6290
dogukanbayrak6767@gmail.com <dogukanbayrak6767@gmail.com>
2022-12-10 23:27:28 +0300

    UI buttons fixed

---
+++ Assets/Scripts/UI/SkillChart.cs
@@ -0,0 +1,321 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillChart : MonoBehaviour
+{
+    
+
+    public List<bool> askeriList = new List<bool>();
+    public List<bool> defansifList = new List<bool>();
+    public List<bool> ozelList = new List<bool>();
+
+    #region DefansButton
+
+    public void DefansButton1()
+    {
+        if (!defansifList[0])
+        {
+           
+            defansifList[0] = true;
+        }
+    }
+    public void DefansButton2()
+    {
+        if (!defansifList[1])
+        {
+            
+            defansifList[1] = true;
+        }
+    }
+    public void DefansButton3()
+    {
+        if (!defansifList[2])
+        {
+            
+            defansifList[2] = true;
+        }
+
+    }
+    public void DefansButton4()
+    {
+        if (!defansifList[3])
+        {
+            
+            defansifList[3] = true;
+        }
+    }
+    public void DefansButton5()
+    {
+        if (!defansifList[4])
+        {
+           
+            defansifList[4] = true;
+        }
+    }
+    public void DefansButton6()
+    {
+        if (!defansifList[5])
+        {
+            
+            defansifList[5] = true;
+        }
+    }
+    public void DefansButton7()
+    {
+        if (!defansifList[6])
+        {
+            
+            defansifList[6] = true;
+        }
+
+    }
+    public void DefansButton8()
+    {
+        if (!defansifList[7])
+        {
+
+            defansifList[7] = true;
+        }
+    }
+    public void DefansButton9()
+    {
+        if (!defansifList[8])
+        {
+
+            defansifList[8] = true;
+        }
+    }
+    public void DefansButton10()
+    {
+        if (!defansifList[9])
+        {
+
+            defansifList[9] = true;
+        }
+    }
+    public void DefansButton11()
+    {
+        if (!defansifList[10])
+        {
+
+            defansifList[10] = true;
+        }
+
+    }
+    public void DefansButton12()
+    {
+        if (!defansifList[11])
+        {
+
+            defansifList[11] = true;
+        }
+    }
+
+    #endregion
+
+    #region Askeri
+    public void AskeriButton1()
+    {
+        if (!askeriList[0])
+        {
+            Debug.Log("denemee");
+            askeriList[0] = true;
+        }
+        
+    }
+    public void AskeriButton2()
+    {
+        if (!askeriList[1])
+        {
+            Debug.Log("denemee");
+            askeriList[1] = true;
+        }
+    }
+    public void AskeriButton3()
+    {
+        if (!askeriList[2])
+        {
+            Debug.Log("denemee");
+            askeriList[2] = true;
+        }
+
+    }
+    public void AskeriButton4()
+    {
+        if (!askeriList[3])
+        {
+            Debug.Log("denemee");
+            askeriList[3] = true;
+        }
+    }
+    public void AskeriButton5()
+    {
+        if (!askeriList[4])
+        {
+            Debug.Log("denemee");
+            askeriList[4] = true;
+        }
+    }
+    public void AskeriButton6()
+    {
+        if (!askeriList[5])
+        {
+            Debug.Log("denemee");
+            askeriList[5] = true;
+        }
+    }
+    public void AskeriButton7()
+    {
+        if (!askeriList[6])
+        {
+            Debug.Log("denemee");
+            askeriList[6] = true;
+        }
+
+    }
+    public void AskeriButton8()
+    {
+        if (!askeriList[7])
+        {
+            Debug.Log("denemee");
+            askeriList[7] = true;
+        }
+    }
+    public void AskeriButton9()
+    {
+        if (!askeriList[8])
+        {
+            Debug.Log("denemee");
+            askeriList[8] = true;
+        }
+    }
+    public void AskeriButton10()
+    {
+        if (!askeriList[9])
+        {
+            Debug.Log("denemee");
+            askeriList[9] = true;
+        }
+    }
+    public void AskeriButton11()
+    {
+        if (!askeriList[10])
+        {
+            Debug.Log("denemee");
+            askeriList[10] = true;
+        }
+    }
+    public void AskeriButton12()
+    {
+        if (!askeriList[11])
+        {
+            Debug.Log("denemee");
+            askeriList[11] = true;
+        }
+
+    }
+    #endregion
+
+    #region OzelGuc
+    public void OzelButton1()
+    {
+        if (!ozelList[0])
+        {
+
+            ozelList[0] = true;
+        }
+    }
+    public void OzelButton2()
+    {
+        if (!ozelList[1])
+        {
+
+            ozelList[1] = true;
+        }
+    }
+    public void OzelButton3()
+    {
+        if (!ozelList[2])
+        {
+
+            ozelList[2] = true;
+        }
+
+    }
+    public void OzelButton4()
+    {
+        if (!ozelList[3])
+        {
+
+            ozelList[3] = true;
+        }
+    }
+    public void OzelButton5()
+    {
+        if (!ozelList[4])
+        {
+
+            ozelList[4] = true;
+        }
+    }
+    public void OzelButton6()
+    {
+        if (!ozelList[5])
+        {
+
+            ozelList[5] = true;
+        }
+    }
+    public void OzelButton7()
+    {
+        if (!ozelList[6])
+        {
+
+            ozelList[6] = true;
+        }
+
+    }
+    public void OzelButton8()
+    {
+        if (!ozelList[7])
+        {
+
+            ozelList[7] = true;
+        }
+    }
+    public void OzelButton9()
+    {
+        if (!ozelList[8])
+        {
+
+            ozelList[8] = true;
+        }
+    }
+    public void OzelButton10()
+    {
+        if (!ozelList[9])
+        {
+
+            ozelList[9] = true;
+        }
+
+    }
+    public void OzelButton11()
+    {
+        if (!ozelList[10])
+        {
+
+            ozelList[10] = true;
+        }
+    }
+    public void OzelButton12()
+    {
+        if (!ozelList[11])
+        {
+
+            ozelList[11] = true;
+        }
+    }
+    #endregion
+}
