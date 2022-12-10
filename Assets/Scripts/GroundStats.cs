fdabbab5aa32fda5ceb87259125215ebdf4ee7cb
Ayberk Keskin <ayberkeskn@gmail.com>
2022-12-10 20:22:17 +0300

    Revert "Merge branch 'master' into ayberks"
    This reverts commit ffbe5fff905bb8226f1b6b2e6e7aa3d9e350703d, reversing
    changes made to 3715b988d0d23f18910ee9dac983ef8c8bafb68e.

--- Assets/Scripts/GroundStats.cs
+++ Assets/Scripts/GroundStats.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 public class GroundStats : MonoBehaviour
@@ -8,25 +7,12 @@ public class GroundStats : MonoBehaviour
 
     [SerializeField] private int gold;
     [SerializeField] private int science;
-    public int soldier;
+    [SerializeField] private int soldier;
     [SerializeField] private int color;
     [SerializeField] private int listIndex;
 
     [SerializeField] GroundControl groundControl;
-    [SerializeField] CombatLogic combatLogic;
 
-    [SerializeField] private TextMeshPro GoldText;
-    [SerializeField] private TextMeshPro ScienceText;
-    [SerializeField] private TextMeshPro SoldierText;
-
-    [Header("PopUp")]
-
-    public bool mouseCheck=false;
-    [SerializeField] private GameObject AttackButton;
-    [SerializeField] private GameObject UpgradeButton;
-
-    [Header("Combat")]
-    [SerializeField] private GameObject parentObject;
 
 
     void Awake()
@@ -39,49 +25,11 @@ public class GroundStats : MonoBehaviour
         listIndex = groundControl.GoldList.Count-1;
     }
 
-    private void Start()
-    {
-        AttackButton.SetActive(false);
-        UpgradeButton.SetActive(false);
-    }
-
     void Update()
     {
         groundControl.GoldList[listIndex]=gold;
         groundControl.ScienceList[listIndex]=science;
         groundControl.SoldierList[listIndex] = soldier;
         groundControl.ColorList[listIndex]=color;
-
-
-        GoldText.text = "Gold: "+ gold.ToString();
-        ScienceText.text = "Science: "+science.ToString();
-        SoldierText.text = "Soldier: "+soldier.ToString();
-
-        PopUp();
-    }
-
-    void PopUp()
-    {
-        if (mouseCheck)
-        {
-            AttackButton.SetActive(true);
-            UpgradeButton.SetActive(true);
-        }
-        else
-        {
-            AttackButton.SetActive(false);
-            UpgradeButton.SetActive(false);
-        }
-    }
-
-    public void AttackSystem()
-    {
-        combatLogic.Attacker = transform.gameObject;
-        combatLogic.attackerSoldier = soldier;
-    }
-    public void DefendSystem()
-    {
-        combatLogic.Defender = transform.gameObject;
-        combatLogic.defenderSoldier = soldier;
     }
 }
